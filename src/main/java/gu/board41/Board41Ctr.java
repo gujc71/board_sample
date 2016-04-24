f0c560fcbc194d3cb176a73781fbdef08e6280aa
gujc <gujc71@gmail.com>
2016-04-24 10:47:41 +0900

    Checkstyle

---
+++ src/main/java/gu/board41/Board41Ctr.java
@@ -0,0 +1,99 @@
+package gu.board41;
+
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import gu.common.FileUtil;
+import gu.common.FileVO;
+import gu.common.SearchVO;
+
+@Controller 
+public class Board41Ctr {
+
+    @Autowired
+    private Board41Svc boardSvc;
+    
+    /**
+     * 리스트.
+     */
+    @RequestMapping(value = "/board41List")
+    public String boardList(SearchVO searchVO, ModelMap modelMap) throws Exception {
+
+        searchVO.PageCalculate( boardSvc.selectBoardCount(searchVO) ); // startRow, endRow
+
+        List<?> listview   = boardSvc.selectBoardList(searchVO);
+        
+        modelMap.addAttribute("listview", listview);
+        modelMap.addAttribute("searchVO", searchVO);
+        return "board4/boardList";
+    }
+    
+    /** 
+     * 글 쓰기. 
+     */
+    @RequestMapping(value = "/board41Form")
+    public String boardForm(HttpServletRequest request, ModelMap modelMap) throws Exception {
+        String brdno = request.getParameter("brdno");
+        if (brdno != null) {
+            BoardVO boardInfo = boardSvc.selectBoardOne(brdno);
+            List<?> listview = boardSvc.selectBoard4FileList(brdno);
+        
+            modelMap.addAttribute("boardInfo", boardInfo);
+            modelMap.addAttribute("listview", listview);
+        }
+        
+        return "board4/boardForm";
+    }
+    
+    /**
+     * 글 저장.
+     */
+    @RequestMapping(value = "/board41Save")
+       public String boardSave(HttpServletRequest request, BoardVO boardInfo) throws Exception {
+        String[] fileno = request.getParameterValues("fileno");
+        
+        FileUtil fs = new FileUtil();
+        List<FileVO> filelist = fs.SaveAllFiles(boardInfo.getUploadfile());
+
+           boardSvc.insertBoard(boardInfo, filelist, fileno);
+
+        return "redirect:/board41List";
+    }
+
+    /**
+     * 글 읽기.
+     */
+    @RequestMapping(value = "/board41Read")
+    public String board4Read(HttpServletRequest request, ModelMap modelMap) throws Exception {
+        
+        String brdno = request.getParameter("brdno");
+        
+        boardSvc.updateBoard4Read(brdno);
+        BoardVO boardInfo = boardSvc.selectBoardOne(brdno);
+        List<?> listview = boardSvc.selectBoard4FileList(brdno);
+        
+        modelMap.addAttribute("boardInfo", boardInfo);
+        modelMap.addAttribute("listview", listview);
+        
+        return "board4/boardRead";
+    }
+    
+    /**
+     * 글 삭제.
+     */
+    @RequestMapping(value = "/board41Delete")
+    public String boardDelete(HttpServletRequest request) throws Exception {
+        
+        String brdno = request.getParameter("brdno");
+        
+        boardSvc.deleteBoardOne(brdno);
+        
+        return "redirect:/board41List";
+    }
+
+}
---
+++ src/main/java/gu/board41/Board41Svc.java
@@ -0,0 +1,80 @@
+package gu.board41;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+import gu.common.FileVO;
+import gu.common.SearchVO;
+
+@Service
+public class Board41Svc {
+
+    @Autowired
+    private SqlSessionTemplate sqlSession;    
+    @Autowired
+    private DataSourceTransactionManager txManager;
+        
+    public Integer selectBoardCount(SearchVO param) throws Exception {
+        return sqlSession.selectOne("selectBoard4Count", param);
+    }
+    
+    public List<?> selectBoardList(SearchVO param) throws Exception {
+        return sqlSession.selectList("selectBoard4List", param);
+    }
+    
+    /**
+     * 글 저장.
+     */
+    public void insertBoard(BoardVO param, List<FileVO> filelist, String[] fileno) throws Exception {
+        
+        DefaultTransactionDefinition def = new DefaultTransactionDefinition();
+        def.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+        TransactionStatus status = txManager.getTransaction(def);
+        
+        try {
+            if (param.getBrdno() == null || "".equals(param.getBrdno())) {
+                 sqlSession.insert("insertBoard4", param);
+            } else {
+                sqlSession.update("updateBoard4", param);
+            }
+    
+            HashMap<String, String[]> fparam = new HashMap<String, String[]>();
+            fparam.put("fileno", fileno);
+            sqlSession.insert("deleteBoard4File", fparam);
+            
+            for (FileVO f : filelist) {
+                f.setParentPK(param.getBrdno());
+                    sqlSession.insert("insertBoard4File", f);
+            }
+            txManager.commit(status);
+        } catch (Exception ex) {
+            txManager.rollback(status);
+            throw ex;
+        }            
+    }
+ 
+    public BoardVO selectBoardOne(String param) throws Exception {
+        return sqlSession.selectOne("selectBoard4One", param);
+    }
+    
+    public void updateBoard4Read(String param) throws Exception {
+        sqlSession.insert("updateBoard4Read", param);
+    }
+    
+    public void deleteBoardOne(String param) throws Exception {
+        sqlSession.delete("deleteBoard4One", param);
+    }
+    
+    public List<?> selectBoard4FileList(String param) throws Exception {
+        return sqlSession.selectList("selectBoard4FileList", param);
+    }
+    
+}
